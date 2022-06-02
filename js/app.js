976b68bb65d85d72ea3a13cdc8030d98fea47698
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-06-02 19:44:42 +0300

    js - add initial constants

---
+++ js/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  /* define constants */
+  const form = document.querySelector("#form");
+  const formSection = document.querySelector(".form");
+  const formHeader = document.querySelector(".header");
+
+  const addNewTaskBtn = document.querySelector("#add-new-task-btn");
+  const filterDoneBtn = document.querySelector("#filter-done-btn");
+  const filterUndoneBtn = document.querySelector("#filter-undone-btn");
+  const removeAllBtn = document.querySelector("#remove-all-btn");
+  const removeFinishedBtn = document.querySelector("#remove-finished-btn");
+  const filtersResetBtn = document.querySelector("#filters-reset-btn");
+
+  const liToClone = document.querySelector("#li-to-clone");
+  const taskList = document.querySelector("#task-list");
+  const priorities = document.querySelector("#filter-priority");
+  const filterPriorityForm = document.querySelector("#filter_priority");
+
+  /* function definitions */
+  // get list from local storage and convert from json to array
+
+  function parseJsonFromLS() {
+    let taskArrayJSON = localStorage.getItem("todolist");
+
+    let htmlArray = [];
+    if (taskArrayJSON) {
+      htmlArray = JSON.parse(taskArrayJSON);
+    }
+    return htmlArray;
+  }
+
+  // get list from local storage and add to html
+  function getFromLS() {
+    let taskArray = parseJsonFromLS();
+    addArrayToHtml(taskArray);
+    findAllButtons();
+  }
+});
