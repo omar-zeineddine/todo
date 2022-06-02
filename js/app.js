82374cbbd7b2b162364480f919b239c2f3467417
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-06-02 21:01:25 +0300

    js - add priorities and form validation

--- js/app.js
+++ js/app.js
@@ -51,4 +51,107 @@ $(document).ready(function () {
       addObjectToHtml(arr[i]);
     }
   }
+
+  // add new tag from object to html
+  function addObjectToHtml(taskObject) {
+    let liCloned = liToClone.cloneNode(true);
+
+    liCloned.classList.remove("hidden-always");
+    liCloned.removeAttribute("id");
+
+    if (taskObject.taskDone) {
+      liCloned.classList.add("done");
+    }
+
+    liCloned.querySelector(".task-name").innerText = taskObject.taskName;
+    liCloned.querySelector(".task-date").innerText = taskObject.taskDate;
+    liCloned.querySelector(".task-priority").innerText =
+      taskObject.taskPriority;
+    liCloned.querySelector(".task-description").innerText =
+      taskObject.taskAbout;
+    liCloned.querySelector(".task-id").innerText = taskObject.taskId;
+
+    taskList.appendChild(liCloned);
+  }
+
+  // validation
+  function generateID() {
+    /* https://qawithexperts.com/questions/242/how-to-generate */
+    return "_" + Math.random().toString(36).substring(6);
+  }
+
+  function formValidateAndSubmit(event) {
+    event.preventDefault();
+
+    let taskInput = document.querySelector("#task-input");
+    let dateInput = document.querySelector("#date-input");
+    let priorities = document.getElementsByName("form-priority");
+    let textAreaInput = document.querySelector("#textarea-input");
+
+    let error = document.querySelector("#error-message");
+    error.innerText = "";
+    let formValid = true;
+
+    let taskPriority;
+    for (let i = 0; i < 5; i++) {
+      if (priorities[i].checked) {
+        taskPriority = priorities[i].value;
+      }
+    }
+
+    let getTaskId = generateID();
+
+    // get a time when a task is added;
+    let getTaskOrder = [];
+    getTaskOrder += new Date();
+    console.log(getTaskOrder);
+
+    // task subject
+
+    if (taskInput.value.length <= 0) {
+      formValid = false;
+      error.innerText += "- enter a task's title \n";
+    }
+    if (taskInput.value.length > 25) {
+      formValid = false;
+      error.innerText += "- a title can't be longer than 25 characters \n";
+    }
+
+    /* priorities */
+    let checkboxCheckedNumber = 0;
+
+    for (let i = 0; i < priorities.length; i++) {
+      if (priorities[i].checked) {
+        checkboxCheckedNumber += 1;
+      }
+    }
+    if (!checkboxCheckedNumber) {
+      error.innerText += "- choose priority \n";
+      formValid = false;
+    }
+
+    //text area
+
+    if (textAreaInput.value.length > 100) {
+      formValid = false;
+      error.innerText +=
+        "- a task's description can't be longer than 100 characters \n";
+    }
+
+    // validate form
+
+    if (formValid) {
+      createNewTaskObject(
+        getTaskId,
+        taskInput.value,
+        dateInput.value,
+        textAreaInput.value,
+        taskPriority
+      );
+      closeForm();
+      form.reset();
+    }
+  }
+
+  form.addEventListener("submit", formValidateAndSubmit);
 });
